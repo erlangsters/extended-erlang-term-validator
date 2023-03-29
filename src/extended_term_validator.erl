1259bb848c14dd911a77940c903a880c060e011b
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-03-29 19:38:49 +0200

    Add LICENSE.txt and source file headers (MIT license)

--- src/extended_term_validator.erl
+++ src/extended_term_validator.erl
@@ -1,7 +1,15 @@
+%%
+%% Copyright (c) 2023, Byteplug LLC.
+%%
+%% This source file is part of the Extended Erlang Term Validator project from
+%% Erlangsters and is released under the MIT license. Please refer to the
+%% LICENSE.txt file that can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, March 2023
+%%
 -module(extended_term_validator).
 
 -export([extended_term_validator/0]).
 
 extended_term_validator() ->
     ok.
-
